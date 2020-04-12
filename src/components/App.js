2c74ac9ffa5a1cb0a655eb37fbbc0e47255f755d
Daniel <50657694+LivingMartialWay@users.noreply.github.com>
2020-04-12 20:06:19 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList'
+
+const KEY = 'AIzaSyAp1CcZzLi66lg_uKb_We8qPOq0lcXWnQ4';
+
+class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit =  async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+      }
+    })
+
+    this.setState({videos : response.data.items });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
